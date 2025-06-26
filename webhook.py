863cd953c962dd9b0ded11d149ec5a9667de0c91
atish41 <123723759+atish41@users.noreply.github.com>
2025-06-26 11:32:10 +0530

    cors

--- webhook.py
+++ webhook.py
@@ -1,10 +1,12 @@
 # from flask t Flask, request, jsonify
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from pprint import pprint
 from app import get_chat
 
 
 app= Flask(__name__)
+CORS(app)
 
 
 
@@ -26,4 +28,4 @@ def chat_sesssion():
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0",port=5000)
+    app.run(debug=True,port=5000)
